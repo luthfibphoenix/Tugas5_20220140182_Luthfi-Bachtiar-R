f509af2d9d8b8e000cad69267e24d5247d411e6a
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-04-01 16:54:35 +0700

    done

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function Video(){
+    alert("ini video Dreamcatcher")
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio Dreamcatcher")
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Dreamcatcher")
+});
